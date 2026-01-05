8d9bdb771e6638b41aa79e911515b3fe41d34c8f
Jacques Dukes <jacques.dukes@inspire11.com>
2026-01-05 10:53:38 -0600

    use relative paths

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -10,23 +10,23 @@
 
 export const IMAGE_URLS: Record<string, string> = {
   // Journey
-  journey_city: "/img/journey_city.jpg",
-  journey_outskirts: "/img/journey_outskirts.jpg",
-  journey_wilds: "/img/journey_wild.jpg",
+  journey_city: "./img/journey_city.jpg",
+  journey_outskirts: "./img/journey_outskirts.jpg",
+  journey_wilds: "./img/journey_wild.jpg",
 
   // Mode
-  mode_kinetic: "/img/mode_kinetic.png",
-  mode_witnessing: "/img/mode_witness.jpg",
-  mode_creative: "/img/mode_creative.jpg",
-  mode_problem_solving: "/img/mode_puzzle.jpg",
+  mode_kinetic: "./img/mode_kinetic.png",
+  mode_witnessing: "./img/mode_witness.jpg",
+  mode_creative: "./img/mode_creative.jpg",
+  mode_problem_solving: "./img/mode_puzzle.jpg",
 
   // Moments
-  moment_change_pace: "/img/moment_change_pace.jpg",
-  moment_commit_ten: "/img/moment_commit_for_ten.jpg",
-  moment_notice_without_fixing: "/img/moment_notice_without_fixing.jpg",
-  moment_third_object: "/img/moment_third_object.jpg",
-  moment_switch_roles: "/img/moment_switch_roles.jpg",
-  moment_add_constraint: "/img/moment_add_constraint.jpg",
+  moment_change_pace: "./img/moment_change_pace.jpg",
+  moment_commit_ten: "./img/moment_commit_for_ten.jpg",
+  moment_notice_without_fixing: "./img/moment_notice_without_fixing.jpg",
+  moment_third_object: "./img/moment_third_object.jpg",
+  moment_switch_roles: "./img/moment_switch_roles.jpg",
+  moment_add_constraint: "./img/moment_add_constraint.jpg",
 
   // Transitions
   // transition_long_rest: "",
